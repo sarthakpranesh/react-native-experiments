285a66c55025fceba8a068f987cc4719f9257f5e
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-05-07 14:42:21 +0530

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AnimatedCube from './src/AnimatedCube';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <AnimatedCube />
   );
 }
 
